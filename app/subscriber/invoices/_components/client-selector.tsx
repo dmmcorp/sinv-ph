1c532521679ab90f2ff19d8db0453a26a1846648
ReformaMark <mark.reforma0117@gmail.com>
2025-12-18 08:48:28 +0800

    feat: improve UI/Ux for selection of clients

--- app/subscriber/invoices/_components/client-selector.tsx
+++ app/subscriber/invoices/_components/client-selector.tsx
@@ -1,23 +1,15 @@
 "use client";
 
-import { useState, useMemo } from "react";
-import { Search, UserPlus, Check, Mail, MapPin, User } from "lucide-react";
+import { useState, useMemo, useEffect } from "react";
+import { Search, UserPlus, Check, ChevronRight } from "lucide-react";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import { Label } from "@/components/ui/label";
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-} from "@/components/ui/dialog";
 import useClientSelection from "@/stores/client/useClientSelection";
 import { useMutation, useQuery } from "convex/react";
 import { api } from "@/convex/_generated/api";
 import { toast } from "sonner";
+import AddNewClientDialog from "./add-new-client-dialog";
 
 export interface Client {
   id: string;
@@ -26,45 +18,25 @@ export interface Client {
   address: string;
 }
 
-// Sample clients data
-const initialClients: Client[] = [
-  {
-    id: "1",
-    name: "John Smith",
-    email: "john@example.com",
-    address: "123 Main St, New York, NY",
-  },
-  {
-    id: "2",
-    name: "Sarah Johnson",
-    email: "sarah@company.co",
-    address: "456 Oak Ave, Los Angeles, CA",
-  },
-  {
-    id: "3",
-    name: "Michael Chen",
-    email: "m.chen@business.com",
-    address: "789 Pine Rd, Chicago, IL",
-  },
-  {
-    id: "4",
-    name: "Emily Davis",
-    email: "emily.d@corp.net",
-    address: "321 Elm St, Houston, TX",
-  },
-];
 interface ClientSelector {
   onSetStep: (step: number) => void;
+}
+export interface NewClientTypes {
+  name: string;
+  email: string;
+  address: string;
+  tin: string;
 }
 export function ClientSelector({ onSetStep }: ClientSelector) {
   const clients = useQuery(api.clients.getAllClients);
   const createClient = useMutation(api.clients.createClient);
+  const clientStore = useClientSelection();
   const [searchQuery, setSearchQuery] = useState("");
-  const [showAddDialog, setShowAddDialog] = useState(false);
-  const [newClient, setNewClient] = useState({
+  const [newClient, setNewClient] = useState<NewClientTypes>({
     name: "",
     email: "",
     address: "",
+    tin: "",
   });
   const { selectedClient, setSelectedClient } = useClientSelection();
 
@@ -91,13 +63,17 @@ export function ClientSelector({ onSetStep }: ClientSelector) {
       address: newClient.address,
     });
 
-    if (result.includes("Successfully created a new client.")) {
-      toast.success(result);
+    if (result.message.includes("Successfully created a new client.")) {
+      toast.success(result.message);
+      //automatically set the newly created client as selected client
+      if (result.data) {
+        setSelectedClient(result.data);
+      }
     } else {
-      toast.error(result);
+      toast.error(result.message);
     }
-    setNewClient({ name: "", email: "", address: "" });
-    setShowAddDialog(false);
+    setNewClient({ name: "", email: "", address: "", tin: "" });
+    clientStore.setNewClientDialogOpen(false);
     setSearchQuery("");
   };
 
@@ -108,10 +84,35 @@ export function ClientSelector({ onSetStep }: ClientSelector) {
     }
   };
 
+  useEffect(() => {
+    const hanleNoClients = () => {
+      if (clients?.length === 0) {
+        if (clientStore.newClientDialogOpen != true) {
+          clientStore.setNewClientDialogOpen(true);
+        }
+      }
+    };
+    hanleNoClients();
+  }, [clients?.length, clientStore]);
+
   return (
     <Card className="flex-1 min-h-full flex flex-col w-full max-w-full shadow-lg">
       <CardHeader className="pb-4">
-        <CardTitle className="text-xl font-semibold">Select a Client</CardTitle>
+        <div className="flex justify-start lg:justify-between items-center">
+          <CardTitle className="">
+            <h1 className="text-2xl">Select a Client</h1>
+          </CardTitle>
+          {/* Add New Client Dialog */}
+
+          <Button
+            variant="outline"
+            className=" hidden lg:flex font-black  text-sm  h-11 lg bg-accent text-white"
+            onClick={() => clientStore.setNewClientDialogOpen(true)}
+          >
+            <UserPlus className="size-7 lg:text-8" />
+            Add New Client
+          </Button>
+        </div>
         <p className="text-sm text-muted-foreground">
           Choose an existing client or add a new one to continue
         </p>
@@ -133,32 +134,33 @@ export function ClientSelector({ onSetStep }: ClientSelector) {
           {filteredClients && filteredClients?.length > 0 ? (
             <div className="flex-1 grid grid-cols-1 lg:grid-cols-4 gap-5 space-y-2 w-full overflow-y-auto ">
               {filteredClients?.map((client) => (
-                <button
-                  key={client._id}
-                  onClick={() => setSelectedClient(client)}
-                  className={`max-h-40 w-full text-left p-3 rounded-lg border transition-all flex flex-col ${
-                    selectedClient?._id === client._id
-                      ? "border-primary bg-primary/5 ring-1 ring-primary"
-                      : "border-border hover:border-primary/50 hover:bg-muted/50"
-                  }`}
-                >
-                  <div className="flex items-start justify-between gap-2 ">
-                    <div className="space-y-1 min-w-0">
-                      <p className="font-semibold truncate">{client.name}</p>
-                      <p className="text-sm text-muted-foreground truncate">
-                        {client.email}
-                      </p>
-                      <p className="text-xs text-muted-foreground truncate">
-                        {client.address}
-                      </p>
-                    </div>
-                    {selectedClient?._id === client._id && (
-                      <div className="shrink-0 h-5 w-5 rounded-full bg-primary flex items-center justify-center">
-                        <Check className="h-3 w-3 text-primary-foreground" />
+                <div key={client._id} className="">
+                  <button
+                    onClick={() => setSelectedClient(client)}
+                    className={`h- w-full text-left p-3 rounded-lg border transition-all flex flex-col ${
+                      selectedClient?._id === client._id
+                        ? "border-primary bg-primary/5 ring-1 ring-primary"
+                        : "border-border hover:border-primary/50 hover:bg-muted/50"
+                    }`}
+                  >
+                    <div className="flex items-start justify-between gap-2 ">
+                      <div className="space-y-1 min-w-0">
+                        <p className="font-semibold truncate">{client.name}</p>
+                        <p className="text-sm text-muted-foreground truncate">
+                          {client.email}
+                        </p>
+                        <p className="text-xs text-muted-foreground truncate">
+                          {client.address}
+                        </p>
                       </div>
-                    )}
-                  </div>
-                </button>
+                      {selectedClient?._id === client._id && (
+                        <div className="shrink-0 h-5 w-5 rounded-full bg-primary flex items-center justify-center">
+                          <Check className="h-3 w-3 text-primary-foreground" />
+                        </div>
+                      )}
+                    </div>
+                  </button>
+                </div>
               ))}
             </div>
           ) : (
@@ -167,7 +169,8 @@ export function ClientSelector({ onSetStep }: ClientSelector) {
                 <UserPlus className="h-6 w-6 text-muted-foreground" />
               </div>
               <h3 className="font-medium mb-1">
-                No client found for &qout;{searchQuery}&qout;
+                No client found for &quot;
+                <span className="font-semibold">{searchQuery}</span> &quot;
               </h3>
               <p className="text-sm text-muted-foreground mb-4">
                 Looks like this client isn&apos;t in your list yet. Would you
@@ -176,7 +179,7 @@ export function ClientSelector({ onSetStep }: ClientSelector) {
               <Button
                 onClick={() => {
                   setNewClient((prev) => ({ ...prev, name: searchQuery }));
-                  setShowAddDialog(true);
+                  clientStore.setNewClientDialogOpen(true);
                 }}
                 size="sm"
               >
@@ -189,109 +192,34 @@ export function ClientSelector({ onSetStep }: ClientSelector) {
         <div className="mt-auto space-y-2">
           {/* Add New Client Button */}
 
+          {/* Add New Client Dialog */}
           {!showNoResults && (
             <Button
-              variant="outline"
-              className="w-full bg-transparent"
-              onClick={() => setShowAddDialog(true)}
+              variant="ghost"
+              className="flex w-full lg:hidden font-semibold text-sm lg:text-2xl h-11"
+              onClick={() => clientStore.setNewClientDialogOpen(true)}
             >
-              <UserPlus className="h-4 w-4 mr-2" />
+              <UserPlus className="size-7 lg:text-8" />
               Add New Client
             </Button>
           )}
 
           {/* Continue Button */}
           <Button
-            className="w-full "
+            className=" w-full font-semibold text-xs sm:text-sm lg:text-2xl h-11"
             disabled={!selectedClient}
             onClick={handleContinue}
           >
-            Continue with Selected Client
+            Continue with Selected Client{" "}
+            <ChevronRight size={40} className="hidden sm:blick size-11" />
           </Button>
         </div>
       </CardContent>
-
-      {/* Add New Client Dialog */}
-      <Dialog open={showAddDialog} onOpenChange={setShowAddDialog}>
-        <DialogContent>
-          <DialogHeader>
-            <DialogTitle>Add New Client</DialogTitle>
-            <DialogDescription>
-              Enter the client&apos;s details below to add them to your list.
-            </DialogDescription>
-          </DialogHeader>
-          <div className="space-y-4 py-4">
-            <div className="space-y-2">
-              <Label htmlFor="name">Name</Label>
-              <div className="relative">
-                <User className="absolute left-3 top-1/2 -translate-y-1/2 h-4 w-4 text-muted-foreground" />
-                <Input
-                  id="name"
-                  placeholder="Client name"
-                  value={newClient.name}
-                  onChange={(e) =>
-                    setNewClient((prev) => ({
-                      ...prev,
-                      name: e.target.value,
-                    }))
-                  }
-                  className="pl-9"
-                />
-              </div>
-            </div>
-            <div className="space-y-2">
-              <Label htmlFor="email">Email</Label>
-              <div className="relative">
-                <Mail className="absolute left-3 top-1/2 -translate-y-1/2 h-4 w-4 text-muted-foreground" />
-                <Input
-                  id="email"
-                  type="email"
-                  placeholder="client@example.com"
-                  value={newClient.email}
-                  onChange={(e) =>
-                    setNewClient((prev) => ({
-                      ...prev,
-                      email: e.target.value,
-                    }))
-                  }
-                  className="pl-9"
-                />
-              </div>
-            </div>
-            <div className="space-y-2">
-              <Label htmlFor="address">Address</Label>
-              <div className="relative">
-                <MapPin className="absolute left-3 top-1/2 -translate-y-1/2 h-4 w-4 text-muted-foreground" />
-                <Input
-                  id="address"
-                  placeholder="123 Street, City, State"
-                  value={newClient.address}
-                  onChange={(e) =>
-                    setNewClient((prev) => ({
-                      ...prev,
-                      address: e.target.value,
-                    }))
-                  }
-                  className="pl-9"
-                />
-              </div>
-            </div>
-          </div>
-          <DialogFooter>
-            <Button variant="outline" onClick={() => setShowAddDialog(false)}>
-              Cancel
-            </Button>
-            <Button
-              onClick={handleAddClient}
-              disabled={
-                !newClient.name || !newClient.email || !newClient.address
-              }
-            >
-              Add Client
-            </Button>
-          </DialogFooter>
-        </DialogContent>
-      </Dialog>
+      <AddNewClientDialog
+        newClient={newClient}
+        onSetNewClient={setNewClient}
+        onHandleAddClient={handleAddClient}
+      />
     </Card>
   );
 }
--- convex/clients.ts
+++ convex/clients.ts
@@ -3,56 +3,53 @@ import { mutation, query } from "./_generated/server";
 import { ConvexError, v } from "convex/values";
 
 export const createClient = mutation({
-    args: {
-        name: v.string(),
-        email: v.optional(v.string()),
-        address: v.optional(v.string()),
-    },
-    handler: async (ctx, {
-        name,
-        address,
-        email
-    }) => {
-        const userId = await getAuthUserId(ctx)
-        if (!userId) throw new ConvexError("Not authenticated")
+  args: {
+    name: v.string(),
+    email: v.optional(v.string()),
+    address: v.optional(v.string()),
+  },
+  handler: async (ctx, { name, address, email }) => {
+    const userId = await getAuthUserId(ctx);
+    if (!userId) throw new ConvexError("Not authenticated");
 
-        const client = await ctx.db.insert("clients", {
-            userId,
-            name,
-            address,
-            email,
-        })
+    const client = await ctx.db.insert("clients", {
+      userId,
+      name,
+      address,
+      email,
+    });
 
-        if (client) {
-            return "Successfully created a new client."
-        } else {
-            return "Error, please review the fields."
-        }
+    const data = await ctx.db.get(client); // 12/18/2025
+
+    if (data) {
+      return { data: data, message: "Successfully created a new client." }; // 12/18/2025
+    } else {
+      return { data: data, message: "Error, please review the fields." }; // 12/18/2025
     }
-})
+  },
+});
 
 export const getAllClients = query({
-    handler: async (ctx) => {
-        const userId = await getAuthUserId(ctx)
-        if (!userId) throw new ConvexError("Not authenticated")
+  handler: async (ctx) => {
+    const userId = await getAuthUserId(ctx);
+    if (!userId) throw new ConvexError("Not authenticated");
 
-        return await ctx.db
-            .query("clients")
-            .withIndex("by_user", q => q.eq("userId", userId))
-            .collect()
-    }
-})
+    return await ctx.db
+      .query("clients")
+      .withIndex("by_user", (q) => q.eq("userId", userId))
+      .order("desc") // 12/18/2025
+      .collect();
+  },
+});
 
 export const getClient = query({
-    args: {
-        clientId: v.id("clients")
-    },
-    handler: async (ctx, { clientId }) => {
-        const userId = await getAuthUserId(ctx)
-        if (!userId) throw new ConvexError("Not authenticated")
+  args: {
+    clientId: v.id("clients"),
+  },
+  handler: async (ctx, { clientId }) => {
+    const userId = await getAuthUserId(ctx);
+    if (!userId) throw new ConvexError("Not authenticated");
 
-        return await ctx.db
-            .get(clientId)
-
-    }
-})
+    return await ctx.db.get(clientId);
+  },
+});
